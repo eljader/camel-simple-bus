7ae110555a56a2f40fee304ff4a6f2e29728a91a
jader <pinkyjad@gmail.com>
2015-03-24 03:25:13 +0300

    debugging

--- src/main/java/ru/cinimex/export/ws/SPBusServiceRoutes.java
+++ src/main/java/ru/cinimex/export/ws/SPBusServiceRoutes.java
@@ -11,12 +11,7 @@ public class SPBusServiceRoutes extends RouteBuilder {
     	OutputTransform1 result = new OutputTransform1();   	
     	result.setInterestList(new InterestList());
     	
-    	// endpoint to our CXF webservice
-    	String cxfEndpoint = "cxf://http://localhost:8080/camel-simple-bus/webservices/SPBusService"
-    	        + "?serviceClass=org.apache.camel.example.reportincident.ReportIncidentEndpoint"
-    	        + "&wsdlURL=ru/cinimex/bus/wsdl/report_incident.wsdl";
-    	    	
-    	from(cxfEndpoint)
+        from("cxf:bean:SPBusService")
     		.to("log:input")
 	        .transform(constant(result));
     }
