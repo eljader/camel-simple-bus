b1d7310d6f9610fd830a5e227a27ecf0f725b99d
jader <pinkyjad@gmail.com>
2015-03-31 03:07:20 +0300

    fix xsl

--- src/main/java/ru/cinimex/export/ws/SPBusServiceRoutes.java
+++ src/main/java/ru/cinimex/export/ws/SPBusServiceRoutes.java
@@ -1,25 +1,14 @@
 package ru.cinimex.export.ws;
 
 import org.apache.camel.builder.RouteBuilder;
-import org.apache.camel.language.bean.BeanLanguage;
-import org.apache.camel.model.dataformat.JaxbDataFormat;
-
-import ru.cinimex.bus.OutputTransform1;
 
 public class SPBusServiceRoutes extends RouteBuilder {
 	
     public void configure() throws Exception {        	
-    	JaxbDataFormat jaxb = new JaxbDataFormat();   
-    	jaxb.setPrettyPrint(true);
-        jaxb.setContextPath("ru.cinimex.bus");
-        
-        OutputTransform1 output = new OutputTransform1();
-      
   
         from("cxf:bean:SPBusService")
         	.to("xslt:xslt/transform1.xsl")
-        //	.to("xslt:xslt/transform1_response.xsl")
-        //	.transform(constant(BeanLanguage.bean(beanType, method))
-	    ;
+        	.to("xslt:xslt/transform1-response.xsl")
+        ;
     }
 }
