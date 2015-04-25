69152342331cca6ebdcc538c2624e1d7e0a17f4f
jader <jader@localhost.arnovikov>
2015-04-26 01:33:42 +0300

    add jpa

---
+++ camel-simple-bus/src/main/java/ru/cinimex/csb/model/Transformresult.java
@@ -0,0 +1,42 @@
+package ru.cinimex.csb.model;
+
+import java.io.Serializable;
+import javax.persistence.*;
+
+
+/**
+ * The persistent class for the TRANSFORMRESULT database table.
+ * 
+ */
+@Entity
+@NamedQuery(name="Transformresult.findAll", query="SELECT t FROM Transformresult t")
+public class Transformresult implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private int sid;
+
+	@Lob
+	private String payload;
+
+	public Transformresult() {
+	}
+
+	public int getSid() {
+		return this.sid;
+	}
+
+	public void setSid(int sid) {
+		this.sid = sid;
+	}
+
+	public String getPayload() {
+		return this.payload;
+	}
+
+	public void setPayload(String payload) {
+		this.payload = payload;
+	}
+
+}
